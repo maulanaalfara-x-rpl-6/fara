3f0c3a562a4a5aa1ed4cbce362869d8c37d03299
maulanaalfara-x-rpl-6 <mohammad_salim_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 16:03:00 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,11 @@
+public class Tes {
+    public static void main(String[] args) {
+        System.out.println("nama maulana alfara salim");
+        System.out.println("alamat wlingi-blitar");
+        System.out.println("kelas x rpl 6 ");
+        System.out.println("hobi mengikuti lomba codm");
+        System.out.println("asal sekolah mts salafiyah kasim " );
+        System.out.println("alasan saya sekolah smk telkom malang untuk memiliki bekal ilmu untuk ke jenjang selanjut nya");
+        System.out.println("sekian dari saya,thanks for reading :) ");
+    }
+}
